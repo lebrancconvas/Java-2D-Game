2b8d8ee0a60d24c240721c4f695348dcb89ad971
lebrancconvas <lebrancconvas@gmail.com>
2022-04-06 16:41:27 +0700

    ✨ Create Basic WIndow.

--- BasicGame/src/App.java
+++ BasicGame/src/App.java
@@ -1,5 +1,30 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javax.swing.*;
+import java.awt.*;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        String screenTitle = "Just a basic game";
+        JFrame window = new JFrame(screenTitle);
+        
+        // Settings. 
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Game
+        BasicGame basicgame = new BasicGame();
+        window.add(basicgame);
+
+        // Fit the window size around the component. 
+        window.pack();
+        window.setVisible(true);
+    }
+}
+
+class BasicGame extends JPanel
+{
+    public BasicGame()
+    {
+        this.setPreferredSize(new Dimension(800, 600));
     }
 }
