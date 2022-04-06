48498100db13883e7222305ed92b03dd66c53361
lebrancconvas <lebrancconvas@gmail.com>
2022-04-06 22:49:38 +0700

    ✨ Drawing on Canvas.

--- BasicGame/src/App.java
+++ BasicGame/src/App.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.lang.*;
 
 public class App
 {
     public static void main(String[] args)
     {
-        String screenTitle = "Just a basic game";
+        String screenTitle = "Just a Basic Game";
         JFrame window = new JFrame(screenTitle);
         
         // Settings. 
@@ -43,6 +44,8 @@ class BasicGame extends JPanel
 
         myBrush.drawSky();
         myBrush.drawSun();
+        // myBrush.drawMountain();
+        myBrush.drawTree();
     }
 }
 
@@ -51,14 +54,59 @@ class PaintBrush
     private Graphics g;
 
     // Set Color 
-    Color skyblue = new Color(77, 237, 255);
-    Color orangesun = new Color(255, 178, 44);
+    private static final Color skyblue = new Color(77, 237, 255);
+    private static final Color orangesun = new Color(255, 178, 44);
 
+    // Init Paint Brush. 
     public PaintBrush(Graphics graphics)
     {
         g = graphics;
     }
 
+    // Shape Brush. 
+    public void brushPolygon(Color color, int x1, int y1, int x2, int y2, int x3, int y3)
+    {
+        g.setColor(color);
+        
+        Polygon polygon = new Polygon();
+
+        polygon.addPoint(x1, y1);
+        polygon.addPoint(x2, y2);
+        polygon.addPoint(x3, y3);
+
+        g.fillPolygon(polygon);
+    }
+    
+    public void brushCircle(Color color, int xAxis, int yAxis, int radius)
+    {
+        g.setColor(color);
+        g.fillOval(xAxis, yAxis, radius, radius);
+    }
+
+    public void brushSquare(Color color, int xAxis, int yAxis, int width)
+    {
+        g.setColor(color);
+        g.fillRect(xAxis, yAxis, width, width); 
+    }
+
+    public void brushRect(Color color, int xAxis, int yAxis, int width, int height)
+    {
+        g.setColor(color);
+        g.fillRect(xAxis, yAxis, width, height);
+    }
+
+    public void brushIsoTriangle(Color color, int xAxis, int yAxis, int base, int height)
+    {
+        brushPolygon(color, xAxis, yAxis, xAxis - base/2, yAxis + height, xAxis + base/2, yAxis + height);
+    }
+
+    public void brushEquiTriangle(Color color, int xAxis, int yAxis, int width)
+    {
+        int height = (int) Math.sqrt((width * width) - ((width / 2) * (width * 2)));
+        brushPolygon(color, xAxis, yAxis, xAxis - width/2, yAxis + height, xAxis + width/2, yAxis + height);
+    }
+
+    // Drawing Stuff. 
     public void drawSky()
     {
         g.setColor(skyblue);
@@ -67,8 +115,31 @@ class PaintBrush
 
     public void drawSun()
     {
-        int radius = 100;
-        g.setColor(orangesun);
-        g.fillOval(500, 70, radius, radius);
+        brushCircle(orangesun, 500, 70, 100);
+    }
+
+    public void drawMountain()
+    {
+        brushPolygon(Color.ORANGE, 10, 10, 5, 15, 100, 100);
+    }
+
+    public void drawTree()
+    {
+        final int leafX = 320, leafY = 300;
+        final int rootX = 300, rootY = 450;
+
+        final int rootWidth = (leafX - rootX) * 2;
+        final int rootHeight = 70;
+
+        final int leafWidth = 100;
+        final int leafHeight = rootY - leafY;
+
+        final boolean validRoot = (leafX > rootX) && (rootX > leafX - leafWidth/2);
+        
+        if(validRoot && (leafY < rootY))
+        {
+            brushRect(Color.RED, rootX, rootY, rootWidth, rootHeight); // Draw Root
+            brushIsoTriangle(Color.GREEN, leafX, leafY, leafWidth, leafHeight); // Draw Leaf 
+        }
     }
 }
