97e5c3ff03510147668ae898aec2913dac6d4de2
lebrancconvas <lebrancconvas@gmail.com>
2022-04-07 20:25:35 +0700

    ✨ Create Window.

--- MechMeow/src/App.java
+++ MechMeow/src/App.java
@@ -1,5 +1,32 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javax.swing.*;
+
+public class App
+{
+    private static void initWindow()
+    {
+        String windowTitle = "Mech Meow";
+        JFrame window = new JFrame(windowTitle);
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        MechMeow mechmeow = new MechMeow();
+        window.add(mechmeow);
+
+        window.setResizable(false);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(new Runnable()
+            {
+                public void run()
+                {
+                    initWindow();
+                }
+            }
+        );
     }
 }
---
+++ MechMeow/src/MechMeow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.lang.*;
+import java.io.*;
+import javax.imageio.*;
+
+class MechMeow extends JPanel
+{
+	public MechMeow()
+	{
+		setPreferredSize(new Dimension(800, 600));
+	}
+}
